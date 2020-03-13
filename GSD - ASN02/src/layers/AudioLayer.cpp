5504a3d9339101c7742a687cc1832230206e312f
Roderick Montague <roderick.montague243@gmail.com>
2020-03-13 19:32:36 -0400

    Positioning Test
    I made a positioning algorithm for the spatial sound using an easing in lerp function.

--- GSD - ASN02/src/layers/AudioLayer.cpp
+++ GSD - ASN02/src/layers/AudioLayer.cpp
@@ -1,5 +1,8 @@
 #include "AudioLayer.h"
 #include "AudioEngine.h"
+#include "florp/app/Application.h"
+#include "florp/app/Window.h"
+#include "florp/app/Timing.h"
 
 void AudioLayer::Initialize()
 {
@@ -9,8 +12,9 @@ void AudioLayer::Initialize()
 	audioEngine.Init(); // getting the singleton.
 	audioEngine.LoadBank("Master");
 
-	audioEngine.LoadEvent("Car Crash");
-	audioEngine.PlayEvent("Car Crash");
+	audioEngine.LoadEvent(audioEvent);
+	// audioEngine.PlayEvent(audioEvent);
+	AudioEngine::GetInstance().SetEventPosition(audioEvent, startPos);
 }
 
 void AudioLayer::Shutdown()
@@ -20,8 +24,83 @@ void AudioLayer::Shutdown()
 	AudioEngine::GetInstance().Shutdown();
 }
 
+// linerar interpolation
+float AudioLayer::Lerp(float a, float b, float u) { return glm::mix(a, b, u); }
+
+// ease in interpolation. The higher the exponent, the greater the ease in and steeper the curve.
+// see the reference for comparisons
+float AudioLayer::EaseInLerp(float a, float b, float u)
+{
+	// y = x^n
+	return glm::mix(a, b, powf(u, 5));
+}
+
+// ease out interpolation
+float AudioLayer::EaseOutLerp(float a, float b, float u)
+{
+	// y = 1 - (1 - x)^n
+	return glm::mix(a, b, 1.0F - powf(1.0F - u, 8));
+}
+
+// ease in and out
+float AudioLayer::EaseInOutLerp(float a, float b, float u)
+{
+	float t = (u < 0.5F) ? 
+		128.0F * (pow(u,8)) :
+		0.5F + (1.0F - powf((2.0F * (1.0F - u)), 8)) / 2.0F;
+
+	return glm::mix(a, b, t);
+}
+
 void AudioLayer::Update()
 {
-	//TODO: Update AudioEngine
-	AudioEngine::GetInstance().Update();
+	using namespace florp::app;
+
+	Window::Sptr window = Application::Get()->GetWindow();
+	float deltaTime = Timing::DeltaTime;
+	AudioEngine& audioEngine = AudioEngine::GetInstance();
+
+	// key presses
+	{
+		// plays the event
+		if (window->IsKeyDown(Key::P))
+		{
+			audioEngine.PlayEvent(audioEvent);
+			playing = true;
+
+			elapsedTime = 0.0F;
+			u = 0.0F; // time value
+			currPos = glm::vec3();
+		}
+	}
+
+	// checks to see if the event is still playing
+	// this function doesn't work for some reason.
+	// playing = audioEngine.isEventPlaying(audioEvent);
+	// std::cout << playing << std::endl;
+
+	// if the audio is playing
+	if (playing)
+	{
+		elapsedTime += deltaTime;
+		// startup is 3 seconds (whole effect is 3.5 seconds)
+		// ending crash starts 11 seconds in
+
+		// repositing; stops at 11 seconds.
+		// if (elapsedTime < 11000.0F)
+		if (u < 1.0F) // crashed into wall
+		{
+			// currPos.x = EaseInLerp(startPos.x, endPos.x, u);
+			currPos.x = EaseInLerp(startPos.x, endPos.x, u);
+			u += U_INC * deltaTime;
+			AudioEngine::GetInstance().SetEventPosition(audioEvent, currPos);
+		}
+		else
+		{
+			playing = false;
+			// std::cout << "STOP" << std::endl;
+		}
+	}
+
+	audioEngine.Update();
 }
--- GSD - ASN02/src/layers/AudioLayer.h
+++ GSD - ASN02/src/layers/AudioLayer.h
@@ -1,22 +1,62 @@
+/*
+ * Name: Team Bonus Fruit
+	- Kennedy Adams	(100632983)
+	- Jonah Griffin	(100702748)
+	- Nathan Tuck	(100708651)
+	- Stephane Gagnon (100694227)
+	- Roderick "R.J." Montague (100701758)
+ * Date: 03/13/2020
+ * Description: the audio layer, which handles the movement as well.
+ * References: 
+	- https://github.com/eppz/Unity.Library.eppz_easing
+*/
+
 #pragma once
+
 #include "florp/app/ApplicationLayer.h"
 #include "GLM/vec3.hpp"
+#include <string>
 
 class AudioLayer : public florp::app::ApplicationLayer
 {
 public:
 	void Initialize() override;
+	
 	void Shutdown() override;
+	
+	// lerp
+	static float Lerp(float a, float b, float u);
+
+	// ease in interpolation (see reference)
+	static float EaseInLerp(float a, float b, float u);
+
+	// ease out interpolation (see reference)
+	static float EaseOutLerp(float a, float b, float u);
+
+	// ease in and out
+	static float EaseInOutLerp(float a, float b, float u);
+
 	void Update() override;
 
 private:
+	// TODO: add play button for sound.
+	std::string audioEvent = "Car Crash"; // the event for the sound
+	float elapsedTime = 0.0F;
+	bool playing = false;
+
+	// x is left/right, y is up/down, z is in/out of the screen.
+	// this is assuming that the camera orientation is its default, which it may not be.
+
 	// start, end, and current position
-	glm::vec3 startPos;
-	glm::vec3 endPos;
-	glm::vec3 currPos;
+	glm::vec3 startPos = glm::vec3(-1.0F, 0.0F, 0.0F);
+	glm::vec3 endPos = glm::vec3(15.0F, 0.0F, 0.0F);
+	glm::vec3 currPos = glm::vec3();
 
 	// 'u' value for lerp.
-	float u_inc = 0.0F;
+	float u = 0.0F;
+
+	// incrementer
+	const float U_INC = 0.09F;
 
 protected:
 
